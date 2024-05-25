1c2c3fc346346b5a559324239a3135406e6badbd
real-qilinren <citybar111@outlook.com>
2024-05-25 23:27:10 +1000

    Fix register bugs in backend

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,14 +5,14 @@ import User from '../models/User.js';
 /* REGISTER */
 export const register = async (req, res) => {
     try {
-        const {firstname, lastname, email, password, picturePath, friends, location, occupation} = req.body;
+        const {firstName, lastName, email, password, picturePath, friends, location, occupation} = req.body;
 
         const salt = await bcrypt.genSalt();
         const passwordHash = await bcrypt.hash(password, salt);
 
         const newUser = new User({
-            firstname,
-            lastname,
+            firstName,
+            lastName,
             email,
             password: passwordHash,
             picturePath,
